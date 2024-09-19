0ceeb2c452d40ef041ca3debf7cd1b164a705fd8
Valeriy Fokin <k2foxspb@mail.ru>
2024-09-19 11:49:54 +0300

    initial framework

---
+++ run.py
@@ -0,0 +1,10 @@
+from wsgiref.simple_server import make_server
+
+from framework.main import Framework
+from urls import routes, fronts
+
+application = Framework(routes, fronts)
+with make_server('', 8000, application) as httpd:
+    print('Serving on port 8000...')
+    httpd.serve_forever()
+
